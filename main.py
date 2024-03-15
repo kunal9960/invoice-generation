5532851b2e4a9e72c66fd6387bfc72b28b5b0b4a
Kunal <kunald9960@gmail.com>
2024-03-15 19:46:45 +0530

    Add a company name and logo

--- main.py
+++ main.py
@@ -13,31 +13,50 @@ for filepath in filepaths:
     filename = Path(filepath).stem
     invoice, date = filename.split("-")
 
-    pdf.set_font(family="Arial", size=16, style="B")
+    pdf.set_font(family="Arial", size=16, style="B")    # Add dynamic invoice
     pdf.cell(w=50, h=8, txt=f"Invoice number: {invoice}", ln=1)
 
-    pdf.set_font(family="Arial", size=16, style="B")
+    pdf.set_font(family="Arial", size=16, style="B")    # Add dynamic date
     pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
 
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
 
-    # Header
+    # Add a header
     columns = df.columns
     columns =[item.replace("_", " ").title() for item in columns]
-    pdf.set_font(family="Arial", size=10, style="B")
-    pdf.cell(w=30, h=8, txt=columns[0], border=1)
-    pdf.cell(w=70, h=8, txt=columns[1], border=1)
-    pdf.cell(w=40, h=8, txt=columns[2], border=1)
-    pdf.cell(w=30, h=8, txt=columns[3], border=1)
+    pdf.set_font(family="Arial", size=12, style="B")
+    pdf.cell(w=25, h=8, txt=columns[0], border=1)
+    pdf.cell(w=60, h=8, txt=columns[1], border=1)
+    pdf.cell(w=45, h=8, txt=columns[2], border=1)
+    pdf.cell(w=35, h=8, txt=columns[3], border=1)
     pdf.cell(w=25, h=8, txt=columns[4], border=1, ln=1)
 
-    # Rows
+    # Add rows with data
     for index, row in df.iterrows():
-        pdf.set_font(family="Arial", size=10)
-        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
-        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
-        pdf.cell(w=40, h=8, txt=str(row["amount_purchased"]), border=1)
-        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.set_font(family="Arial", size=12)
+        pdf.cell(w=25, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=60, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=45, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=35, h=8, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=25, h=8, txt=str(row["total_price"]), border=1, ln=1)
+
+    # Add total sum
+    total_sum = df["total_price"].sum()
+
+    pdf.set_font(family="Arial", size=12, style="B")
+    pdf.cell(w=25, h=8, txt="", border=1)
+    pdf.cell(w=60, h=8, txt="", border=1)
+    pdf.cell(w=45, h=8, txt="", border=1)
+    pdf.cell(w=35, h=8, txt="", border=1)
+    pdf.cell(w=25, h=8, txt=str(total_sum), border=1, ln=1)
+
+    # Add total sum
+    pdf.set_font(family="Arial", size=15, style="B")
+    pdf.cell(w=25, h=8, txt=f"The total price is {total_sum}", ln=1)
+
+    # Add company name and logo
+    pdf.set_font(family="Arial", size=15, style="B")
+    pdf.cell(w=25, h=8, txt=f"Dev Build")
+    pdf.image("Dev.png", w=12)
 
     pdf.output(f"PDFs/{filename}.pdf")
